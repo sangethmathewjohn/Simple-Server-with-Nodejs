bf600ecc42fd70c3b441e0800cfe4ab1aa4cb3dd
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-10-18 14:22:16 +0530

    Create server-pages.js

---
+++ server-pages.js
@@ -0,0 +1,19 @@
+ const express = require("express");
+ const app = express();
+ app.get("/",function(request,response){
+     console.log(request);
+     response.send("Hello WORLD");
+ });
+ app.get("/contact",function(request,response){
+
+    response.send("Contact page");
+}); 
+app.get("/about",function(request,response){
+    console.log(request);
+    response.send("about page");
+});
+
+
+ app.listen(3000,function(){
+     console.log("Server on port 3000");
+ });
