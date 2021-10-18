f1c6b00775730c6717790546e5dab075f6ac0258
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-10-18 14:48:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,19 @@
      console.log(request);
      response.send("Hello WORLD");
  });
+ app.get("/contact",function(request,response){
+
+    response.send("Contact page");
+}); 
+app.get("/about",function(request,response){
+    console.log(request);
+    response.send("about page");
+});
+app.get("/nodemon",function(request,response){
+    console.log(request);
+    response.send("nodemon page");
+});
+
  app.listen(3000,function(){
      console.log("Server on port 3000");
  });
